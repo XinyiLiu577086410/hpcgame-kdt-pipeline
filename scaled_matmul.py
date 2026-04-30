abd45705cba41492a197186d3b425df92732a5a0
Xinyi Liu <94362768+XinyiLiu577086410@users.noreply.github.com>
2026-04-30 15:24:20 +0800

    perf(task3): 30/30 pts

--- scaled_matmul.py
+++ scaled_matmul.py
@@ -34,8 +34,8 @@ def matmul_kernel(task_args: Dict[str, int], io_tensors: Dict[str, kdt.Tile]):
     Bb_tile = kdt.alloc_spm((NUM_STAGES, BLOCK_SIZE_K, BLOCK_SIZE_N), dtype='float32')
     As_tile = kdt.alloc_spm((NUM_STAGES, BLOCK_SIZE_M, 1), dtype='float32')
     Bs_tile = kdt.alloc_spm((NUM_STAGES, 1, BLOCK_SIZE_N), dtype='float32')
-    scale_temp = kdt.alloc_spm((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype='float32', init_value = 0.0)
-    c_tile_temp = kdt.alloc_spm((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype='float32', init_value = 0.0)
+    scale_temp = kdt.alloc_spm((2, BLOCK_SIZE_M, BLOCK_SIZE_N), dtype='float32', init_value = 0.0)
+    c_tile_temp = kdt.alloc_spm((2, BLOCK_SIZE_M, BLOCK_SIZE_N), dtype='float32', init_value = 0.0)
     c_tile = kdt.alloc_spm((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype='float32', init_value = 0.0)
 
     for stage in range(0, NUM_STAGES - 1):
@@ -51,17 +51,12 @@ def matmul_kernel(task_args: Dict[str, int], io_tensors: Dict[str, kdt.Tile]):
         stage = k % NUM_STAGES
         K_start = k * BLOCK_SIZE_K
         K_end = K_start + BLOCK_SIZE_K
-
-        # kdt.load(io_tensors['Ab'][M_start:M_end, K_start:K_end], Ab_tile[stage, :, :])
-        # kdt.load(io_tensors['Bb'][K_start:K_end, N_start:N_end], Bb_tile[stage, :, :])
-        # kdt.load(io_tensors['As'][M_start:M_end, k:k+1], As_tile[stage, :, :])
-        # kdt.load(io_tensors['Bs'][k:k+1, N_start:N_end], Bs_tile[stage, :, :])
     
-        kdt.matmul(Ab_tile[stage], Bb_tile[stage], c_tile_temp, accumulate = False)
+        kdt.matmul(Ab_tile[stage], Bb_tile[stage], c_tile_temp[k%2], accumulate = False)
         kdt.mul(
             kdt.broadcast_to(As_tile[stage], 1, BLOCK_SIZE_N),
             kdt.broadcast_to(Bs_tile[stage], 0, BLOCK_SIZE_M), 
-            scale_temp
+            scale_temp[k%2]
         )
         next_k = k + NUM_STAGES -1
         next_stage = next_k % NUM_STAGES
@@ -72,11 +67,19 @@ def matmul_kernel(task_args: Dict[str, int], io_tensors: Dict[str, kdt.Tile]):
             kdt.load(io_tensors['Bb'][next_K_start:next_K_end, N_start:N_end], Bb_tile[next_stage, :, :])
             kdt.load(io_tensors['As'][M_start:M_end, next_k:next_k+1], As_tile[next_stage, :, :])
             kdt.load(io_tensors['Bs'][next_k:next_k+1, N_start:N_end], Bs_tile[next_stage, :, :])
-        kdt.fma(
-            scale_temp,
-            c_tile_temp,
-            c_tile,
-            c_tile)
+        if k != 0:
+            kdt.fma(
+                scale_temp[k%2^1],
+                c_tile_temp[k%2^1],
+                c_tile,
+                c_tile
+            )
+    kdt.fma(
+        scale_temp[num_k_blocks%2^1],
+        c_tile_temp[num_k_blocks%2^1],
+        c_tile,
+        c_tile
+    )
     kdt.store(c_tile, io_tensors["C"][M_start:M_end,N_start:N_end])
 
 def get_kernel(task_id: int) -> kdt.KernelFunction:
