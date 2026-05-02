8c97e33d19bf4872478e05827fe809c570f65a75
Xinyi Liu <94362768+XinyiLiu577086410@users.noreply.github.com>
2026-05-02 15:18:51 +0800

    feat(task4): fa v2 style online softmax, 29.7/40 pts

--- flash-attention.py
+++ flash-attention.py
@@ -28,20 +28,19 @@ def flash_attention_kernel(task_args: Dict[str, int], io_tensors: Dict[str, kdt.
     Q_tile = kdt.alloc_spm((BLOCK_SIZE_QO, D), dtype='float32')
     K_tiles = kdt.alloc_spm((NUM_STAGES, BLOCK_SIZE_KV, D), dtype='float32')
     V_tiles = kdt.alloc_spm((NUM_STAGES, BLOCK_SIZE_KV, D), dtype='float32')
-    QK_tile = kdt.alloc_spm((BLOCK_SIZE_QO, BLOCK_SIZE_KV), dtype='float32')
+    QK_score = kdt.alloc_spm((BLOCK_SIZE_QO, BLOCK_SIZE_KV), dtype='float32')
     O_tile = kdt.alloc_spm((BLOCK_SIZE_QO, D), dtype='float32', init_value = 0)
     
     global_rowmax = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32', init_value=inf)
-    global_exp_rowsum = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32', init_value=0)
+    global_rowsum = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32', init_value=0)
 
     local_rowmax = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
-    local_exp_rowsum = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
-    
-    local_rowmax_diff = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
-    global_rowmax_diff = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
+    local_rowsum = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
+
+    scale_online_softmax = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
     
     global_rowmax_new = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
-    global_exp_rowsum_new = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
+    global_rowsum_new = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
 
     tmp1 = kdt.alloc_spm((BLOCK_SIZE_QO,), dtype='float32')
 
@@ -60,7 +59,7 @@ def flash_attention_kernel(task_args: Dict[str, int], io_tensors: Dict[str, kdt.
             kdt.load(K_global[KV_start:KV_end, :], K_tiles[stage, :, :])
             kdt.load(V_global[KV_start:KV_end, :], V_tiles[stage, :, :])
         if kv_id == 0:
-            kdt.matmul(Q_tile, kdt.transpose(K_tiles[0], 0, 1), QK_tile, accumulate=False)
+            kdt.matmul(Q_tile, kdt.transpose(K_tiles[0], 0, 1), QK_score, accumulate=False)
 
     for kv_id in range(0, num_kv_blocks):
         stage = kv_id % NUM_STAGES
@@ -75,47 +74,38 @@ def flash_attention_kernel(task_args: Dict[str, int], io_tensors: Dict[str, kdt.
             kdt.load(K_global[KV_start:KV_end, :], K_tiles[prefetch_stage])
             kdt.load(V_global[KV_start:KV_end, :], V_tiles[prefetch_stage])
         
-        kdt.reduce(QK_tile, 1, 'max', local_rowmax)
-        
-        kdt.sub(QK_tile, kdt.broadcast_to(kdt.unsqueeze(local_rowmax, 1), 1, BLOCK_SIZE_KV), QK_tile)
-        QK_tile_sub = QK_tile
-        kdt.exp(QK_tile_sub, QK_tile_sub, e)
-        QK_tile_sub_exp = QK_tile_sub
-        
-        kdt.matmul(QK_tile_sub_exp, V_tile, V_tile)
-        PV_tile = V_tile
-        
-        # PV_tile = matmul(..., ...) overlap w/ VXM ops under
-        kdt.reduce(QK_tile_sub_exp, 1, 'sum', local_exp_rowsum)
-
+        kdt.reduce(QK_score, 1, 'max', local_rowmax)
         kdt.max(local_rowmax, global_rowmax, global_rowmax_new)
+        
+        kdt.sub(QK_score, kdt.broadcast_to(kdt.unsqueeze(global_rowmax_new, 1), 1, BLOCK_SIZE_KV), QK_score)
+        kdt.exp(QK_score, QK_score, e)
+        kdt.matmul(QK_score, V_tile, V_tile)
+        PV_tile = V_tile
 
-        kdt.sub(global_rowmax, global_rowmax_new, global_rowmax_diff)
-        kdt.sub(local_rowmax, global_rowmax_new, local_rowmax_diff)
-        kdt.exp(local_rowmax_diff, local_rowmax_diff, e)
-        kdt.exp(global_rowmax_diff, global_rowmax_diff, e)
-        global_rowmax_diff_exp = global_rowmax_diff
-        local_rowmax_diff_exp = local_rowmax_diff
-        kdt.mul(global_rowmax_diff_exp, global_exp_rowsum, tmp1)
-        kdt.fma(local_rowmax_diff_exp, local_exp_rowsum, tmp1, global_exp_rowsum_new)
-        kdt.mul(O_tile, kdt.broadcast_to(kdt.unsqueeze(tmp1, 1), 1, D), O_tile)
+        # PV_tile = matmul(..., ...) overlap w/ VXM ops under
+        kdt.reduce(QK_score, 1, 'sum', local_rowsum)
+
+        kdt.sub(global_rowmax, global_rowmax_new, scale_online_softmax)
+        kdt.exp(scale_online_softmax, scale_online_softmax, e)
+        kdt.fma(scale_online_softmax, global_rowsum, local_rowsum, global_rowsum_new)
+        # fa v2 said here should be div?
+        kdt.mul(O_tile, kdt.broadcast_to(kdt.unsqueeze(scale_online_softmax, 1), 1, D), O_tile)
         # PV_tile = matmul(..., ...) will overlap w/ VXM ops above
         
         next_kv_id = kv_id + 1
         if next_kv_id < num_kv_blocks:
             next_stage = next_kv_id % NUM_STAGES
             # type: MXM
-            kdt.matmul(Q_tile, kdt.transpose(K_tiles[next_stage], 0, 1), QK_tile, accumulate=False)
+            kdt.matmul(Q_tile, kdt.transpose(K_tiles[next_stage], 0, 1), QK_score, accumulate=False)
             # Q @ K^T (MXM) of next stage overlap w/ below mul & add & div VXM operations
 
         # type: VXM
-        kdt.mul(PV_tile, kdt.broadcast_to(kdt.unsqueeze(local_rowmax_diff_exp, 1), 1, D), PV_tile)
         kdt.add(O_tile, PV_tile, O_tile)
-        kdt.div(O_tile, kdt.broadcast_to(kdt.unsqueeze(global_exp_rowsum_new, 1), 1, D), O_tile)
         kdt.copy(global_rowmax_new, global_rowmax)
-        kdt.copy(global_exp_rowsum_new, global_exp_rowsum)
+        kdt.copy(global_rowsum_new, global_rowsum)
 
 
+    kdt.div(O_tile, kdt.broadcast_to(kdt.unsqueeze(global_rowsum, 1), 1, D), O_tile)
     kdt.store(O_tile, O_global[Q_start:Q_end, :])
 
 
